1ddfbe0b8d2651bf9b8390424e24a3ab6e3a85d6
Sam Mudick <smudick@gmail.com>
2021-05-28 20:28:01 -0500

    added user-plants to api

--- PlantTracker/PlantTracker/Startup.cs
+++ PlantTracker/PlantTracker/Startup.cs
@@ -47,6 +47,7 @@ namespace PlantTracker
             services.AddTransient<UserRepository>();
             services.AddTransient<PlantDataRepository>();
             services.AddTransient<PlantTypesRepository>();
+            services.AddTransient<UserPlantsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
