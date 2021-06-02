0dcf97e9804b1c6ab76d5a20c372784393784249
Sam Mudick <smudick@gmail.com>
2021-06-01 19:00:31 -0500

    got initial react setup running

---
+++ PlantTracker/planttracker.ui/src/App/App.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './App.scss'; 
+
+class App extends Component {
+  render(): JSX.Element {
+    return <div className="App">
+      <h1>Plant Tracker</h1>
+    </div>;
+  }
+}
+export default App;
