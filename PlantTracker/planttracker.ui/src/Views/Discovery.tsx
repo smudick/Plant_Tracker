9979f8205777ff245538a9dbf289d23bf9b70c4f
Sam Mudick <smudick@gmail.com>
2021-06-18 13:38:07 -0500

    styled quiz page

--- PlantTracker/planttracker.ui/src/Views/Discovery.tsx
+++ PlantTracker/planttracker.ui/src/Views/Discovery.tsx
@@ -140,96 +140,148 @@ class Discovery extends Component<DiscoveryProps> {
       cards = [<h1>something has gone wrong here</h1>];
     }
     return (
-      <div>
+      <div className="quizContainer">
         {questionNumber === 0 && (
           <div>
-            <h1>Welcome to the Plant Discovery Quiz!</h1>
-            <h4>
-              Answering a few simple quesitons will help you find the perfect
-              next plant for your home
+            <h1 className="mb-5">Welcome to the Plant Discovery Quiz!</h1>
+            <h4 className="m-3">
+              Want to find the next plant for your home but aren't sure where to
+              start?
             </h4>
-            <button onClick={() => this.startQuiz()}>Let's go!</button>
+            <h4 className="m-3">
+              Answer a few simple questions, and we'll give you a perfect fit!
+            </h4>
+            <button
+              className="quizStartButton"
+              onClick={() => this.startQuiz()}
+            >
+              Let's go!
+            </button>
           </div>
         )}
         {questionNumber === 1 && (
           <div>
-            <h1>Question 1:</h1>
-            <h3>
-              Think of where you are planning to have this plant in your house,
-              then select which answer best matches your space
+            <h1>Question One</h1>
+            <h3 className="m-5">
+              Which of these best describes the space where this plant will
+              live?
             </h3>
-            <button onClick={() => this.questionOneResponse(1)}>
-              I only have a small space to keep this plant, like a shelf or an
-              end table
-            </button>
-            <button onClick={() => this.questionOneResponse(2)}>
-              I have a decent amount of space for this plant, like a corner of a
-              room
-            </button>
-            <button onClick={() => this.questionOneResponse(3)}>
-              I have plenty of room and could fit a larger plant
-            </button>
+            <div className="quiz">
+              <button
+                className="question"
+                onClick={() => this.questionOneResponse(1)}
+              >
+                I only have a small space to keep this plant, like a shelf or an
+                end table
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionOneResponse(2)}
+              >
+                I have a decent amount of space for this plant, like a corner of
+                a room
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionOneResponse(3)}
+              >
+                I have plenty of room and could fit a larger plant
+              </button>
+            </div>
           </div>
         )}
         {questionNumber === 2 && (
           <div>
-            <h1>Question 2:</h1>
-            <h3>
+            <h1>Question Two</h1>
+            <h3 className="m-5">
               How much natural sunlight is in the space where your plant will
               live?
             </h3>
-            <button onClick={() => this.questionTwoResponse(1)}>
-              Tons! I have large windows that let in a lot of sunlight
-            </button>
-            <button onClick={() => this.questionTwoResponse(2)}>
-              There is some natural light, at some parts of the day, but it's
-              not the brightest
-            </button>
-            <button onClick={() => this.questionTwoResponse(3)}>
-              The room is pretty dim most of the time, and I'd really like a
-              plant that is going to be cool with that
-            </button>
+            <div className="quiz">
+              <button
+                className="question"
+                onClick={() => this.questionTwoResponse(1)}
+              >
+                Tons! I have large windows that let in a lot of sunlight
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionTwoResponse(2)}
+              >
+                There is some natural light, at some parts of the day, but it's
+                not the brightest
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionTwoResponse(3)}
+              >
+                The room is pretty dim most of the time, and I'd really like a
+                plant that is going to be cool with that
+              </button>
+            </div>
           </div>
         )}
         {questionNumber === 3 && (
           <div>
-            <h1>Question 3:</h1>
-            <h3>
-              We love pets and plants and want them to coexist peacefully. Do
-              you have dogs or cats that might try to taste your new house
+            <h1>Question Three</h1>
+            <h3 className="m-5">
+              We love pets and plants and want them to coexist peacefully. <br></br>
+              Do you have dogs or cats that might try to taste your new house
               plant?
             </h3>
-            <button onClick={() => this.questionThreeResponse(1)}>
-              Oh yes, I have both cats and dogs and they get into
-              eeeeeeeeverything
-            </button>
-            <button onClick={() => this.questionThreeResponse(2)}>
-              Yes, I have a cat and I do NOT trust it
-            </button>
-            <button onClick={() => this.questionThreeResponse(3)}>
-              Yes, my dog eats literally anything and everything
-            </button>
-            <button onClick={() => this.questionThreeResponse(4)}>
-              No, I do not have any dogs or cats. OR No, I do have pets but they
-              are perfect angels who never eat plants.
-            </button>
+            <div className="quiz">
+              <button
+                className="question"
+                onClick={() => this.questionThreeResponse(1)}
+              >
+                Oh yes, I have both cats and dogs and they get into
+                eeeeeeeeverything
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionThreeResponse(2)}
+              >
+                Yes, I have a cat and I do NOT trust it
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionThreeResponse(3)}
+              >
+                Yes, my dog eats literally anything and everything
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionThreeResponse(4)}
+              >
+                No, I do not have any dogs or cats. OR No, I do have pets but
+                they are perfect angels who never eat plants.
+              </button>
+            </div>
           </div>
         )}
         {questionNumber === 4 && (
           <div>
-            <h1>Question 4:</h1>
-            <h3>Would you like your plant to have flowers?</h3>
-            <button onClick={() => this.questionFourResponse(1)}>
-              Yes please!
-            </button>
-            <button onClick={() => this.questionFourResponse(2)}>
-              Flowers are fine, but I don't need them
-            </button>
+            <h1>Question Four</h1>
+            <h3 className="m-5">Would you like your plant to have flowers?</h3>
+            <div className="quiz">
+              <button
+                className="question"
+                onClick={() => this.questionFourResponse(1)}
+              >
+                Yes please!
+              </button>
+              <button
+                className="question"
+                onClick={() => this.questionFourResponse(2)}
+              >
+                Flowers are fine, but I don't need them
+              </button>
+            </div>
           </div>
         )}
         {questionNumber === 5 && (
           <div>
-            <h1>
+            <h1 className="m-3">
               Based on your answers, we believe that these plants would be best
               suited to your home!
             </h1>
