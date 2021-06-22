d9bd49ee126b240656e6fe4fa57350f090f63eb0
Sam Mudick <smudick@gmail.com>
2021-06-22 10:37:31 -0500

    creating dev branch with new user info

--- PlantTracker/planttracker.ui/src/Helpers/Interfaces/UserInterface.d.ts
+++ PlantTracker/planttracker.ui/src/Helpers/Interfaces/UserInterface.d.ts
@@ -1,11 +1,10 @@
 declare module 'UserTypes' {
     interface User {
         id: number | null;
-        first_Name: string | null;
-        last_Name: string | null;
         profile_Picture: string | null;
         user_Created_Date: Date | null;
         firebase_Uid: string | null;
+        is_Admin: boolean | null;
     }
     type UserProps = {
         user: User,
